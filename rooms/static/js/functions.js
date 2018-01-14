8093d81be03c7b870a60752df2951f0c09eb61ce
Bruno Gonçalves <bruno.m.goncalves@ist.utl.pt>
2018-01-14 21:00:22 +0000

    done checkout

--- rooms/static/js/functions.js
+++ rooms/static/js/functions.js
@@ -14,15 +14,40 @@ function check_in(room_id, room_name) {
     http.onreadystatechange = function () {
 
         if (this.readyState == 4) {
+            replace('alertModalLabel', "Check-in Status");
             if (this.status == 200) {
-                replace('modal_text', "Successful check-in at " + room_name + ".");
+                replace('alertModalText', "Successful check-in at " + room_name + ".");
             }else{
-                replace('modal_text', "Could not check-in at " + room_name + ".");
+                replace('alertModalText', "Could not check-in at " + room_name + ".");
             }
-            $('#checkInModal').modal('show');
+            $('#alertModal').modal('show');
         }
     };
 
     http.send(params);
+
+}
+
+function check_out() {
+    var http = new XMLHttpRequest();
+    var url = "/room4u/check-out";
+
+    http.open("POST", url, true);
+    http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+
+    http.onreadystatechange = function () {
+
+        if (this.readyState == 4) {
+            replace('alertModalLabel', "Check-out Status");
+            if (this.status == 200) {
+                replace('alertModalText', "Checked-out with success.");
+            }else{
+                replace('alertModalText', "Could not check-out.");
+            }
+            $('#alertModal').modal('show');
+        }
+    };
+
+    http.send();
 
 }
