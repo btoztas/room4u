b6c42ba25f152ba85ff7001618c335f930e12798
Bruno Gonçalves <bruno.m.goncalves@ist.utl.pt>
2018-01-28 13:45:47 +0000

    urls

--- rooms/urls.py
+++ rooms/urls.py
@@ -23,7 +23,7 @@ urlpatterns = [
     url(r'^check-out', csrf_exempt(CheckOutView.as_view())),
     url(r'^rooms/$', RoomsView.as_view()),
     url(r'^rooms/reload$', RoomsReloadView.as_view()),
-    url(r'^rooms/(?P<room_id>\d+)/$', cache_page(10)(RoomView.as_view())),
+    url(r'^rooms/(?P<room_id>\d+)/$', cache_page(60)(RoomView.as_view())),
     url(r'^users/$', UsersView.as_view()),
     url(r'^users/(?P<username>.*)/$', cache_page(15*60)(UserView.as_view())),
     url(r'^admin-panel/$', cache_page(15*60)(auth_views.login), {'template_name': 'admin_login.html'}, name='admin_login'),
@@ -31,8 +31,8 @@ urlpatterns = [
 
     # API urls
     url(r'^api/$', ApiView.as_view()),
-    url(r'^api/rooms/$', RoomsApiView.as_view()),
-    url(r'^api/rooms/(?P<room_id>\d+)/$', RoomsApiView.as_view()),
+    url(r'^api/rooms/$',  cache_page(15*60)(RoomsApiView.as_view())),
+    url(r'^api/rooms/(?P<room_id>\d+)/$', cache_page(15*60)(RoomsApiView.as_view())),
     url(r'^api/rooms/(?P<room_id>\d+)/(?P<search>.*)/$', RoomsApiView.as_view()),
     url(r'^api/messages/$', csrf_exempt(MessagesApiView.as_view())),
     url(r'^api/messages/(?P<message_id>\d+)/$', MessagesApiView.as_view()),
@@ -40,8 +40,8 @@ urlpatterns = [
     url(r'^api/visits/(?P<visit_id>\d+)$', csrf_exempt(VisitApiView.as_view())),
     url(r'^api/new_messages$', csrf_exempt(NewMessageApiView.as_view())),
     url(r'^api/new_messages/(?P<new_message_id>\d+)$', csrf_exempt(NewMessageApiView.as_view())),
-    url(r'^api/users$', csrf_exempt(UserApiView.as_view())),
-    url(r'^api/users/(?P<user_id>\d+)$', csrf_exempt(UserApiView.as_view())),
+    url(r'^api/users$', cache_page(15*60)(csrf_exempt(UserApiView.as_view()))),
+    url(r'^api/users/(?P<user_id>\d+)$', cache_page(10)(csrf_exempt(UserApiView.as_view()))),
     url(r'^api/users/(?P<user_id>\d+)/(?P<resource>.*)$', csrf_exempt(UserApiView.as_view())),
 ]
 
